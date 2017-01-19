cafd9f31fd19f846fb474a24c02af5d47488f105
Mihnea Lazar <lzr_mihnea@yahoo.com>
2017-01-19 16:00:13 +0100

    [#INFOTRANS-155] Move alfa in sources/server/back-end - changes after review - removed pom.xml dependency versions which are also defined in parents - added column names for GpsCoordinates used in Stop

--- sources/server/back-end/src/main/java/ro/gov/ithub/entity/util/GpsCoordinate.java
+++ sources/server/back-end/src/main/java/ro/gov/ithub/entity/util/GpsCoordinate.java
@@ -1,14 +1,16 @@
 package ro.gov.ithub.entity.util;
 
+import ro.gov.ithub.entity.Stop;
+
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class GpsCoordinate {
 
-    @Column(nullable = false)
+    @Column(name = Stop.COLUMN_STOP_LATITUDE, nullable = false)
     private Double latitude;
 
-    @Column(nullable = false)
+    @Column(name = Stop.COLUMN_STOP_LONGITUDE, nullable = false)
     private Double longitude;
 }
